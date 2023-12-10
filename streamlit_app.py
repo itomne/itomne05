1f8dd51bbf3ba26e109657ca877ac41107f47ed2
itomne <149847382+itomne@users.noreply.github.com>
2023-12-10 15:51:29 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import os
 import time
 import openai
 import requests
+import pandas as pd
 import streamlit as st
+
 from PIL import Image
 
 
@@ -12,7 +14,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 #会話履歴保存場所
-conversation_history_1 = []  # Global scope
+conversation_history_1 = []  # Global scopes
 conversation_history_2 = []  # Global scope
 conversation_history_3 = []  # Global scope
 conversation_history_4 = []  # Global scope
@@ -21,20 +23,18 @@ conversation_history_5 = []  # Global scope
 def main_page():
     
     st.title('ATAIとの対話型鑑賞')
-    st.write("アート思考力を高めるために対話型鑑賞しましょう。左のサイドバーからあなたが好きな絵を選んでください。好きな絵についてATAI(Art Thinking AI) と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう")
-    st.write("好きな絵についてATAI(Art Thinking AI) と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう")
-    st.write("page1~5をの絵を選択して、対話型鑑賞をやってみましょう！")
+    st.write('<font size="5">アート思考力を高めるために対話型鑑賞しましょう。左のサイドバーからあなたが好きな絵を選んでください。好きな絵についてATAI(Art Thinking AI) と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう。好きな絵についてATAI(Art Thinking AI) と思ったこと/感じたことを話してみましょう。page1~5をの絵を選択して、対話型鑑賞をやってみましょう。</font>', unsafe_allow_html=True)
     image_main = Image.open("総合.png")
     st.image(image_main, width=400)
-    st.write("対話型鑑賞：ニューヨーク近代美術館で生まれた美術教育の新しい方法論。作品を見ながら鑑賞者と教育者で対話しながら「なぜこの絵が気になるのか？」という疑問や感想を用いて、作品の背景にあるものを考察することで、自身の思考を深める教育法です")
+    st.write('<font size="5">対話型鑑賞：ニューヨーク近代美術館で生まれた美術教育の新しい方法論。作品を見ながら鑑賞者と教育者で対話しながら「なぜこの絵が気になるのか？」という疑問や感想を用いて、作品の背景にあるものを考察することで、自身の思考を深める教育法です。</font>', unsafe_allow_html=True)
     
 
 
 def page1():
     prompt = ""  # Initialize your prompt
 
-    st.title('リクリット・ティラバーニャ「“Who’s Afraid of Red, Yellow and Green,」')
-    st.write("好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょ")
+    st.title("リクリット・ティラバーニャ「Who's Afraid of Red, Yellow and Green?」")
+    st.write('<font size="5">好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう。</font>', unsafe_allow_html=True)
     image_1 = Image.open("11.リクリット・ティラバーニャ「“Who’s Afraid of Red, Yellow and Green,」.jpg")
     st.image(image_1, width=400)
 
@@ -58,16 +58,15 @@ def page1():
             data = {
                 "model": "gpt-4",
                 "messages": [
-                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。相手は[ビジネスパーソン]です。"},
-                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、クライアントに問いかけます。"},
-                    {"role": "system", "content": "制約条件:あなたはリクリット・ティラバーニャ「“Who’s Afraid of Red, Yellow and Green,」という作品について、相手とやり取りする"},
+                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。"},
+                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、相手に問いかけます。"},
+                    {"role": "system", "content": "制約条件:あなたはリクリット・ティラバーニャ「Who's Afraid of Red, Yellow and Green?」という作品について、相手とやり取りする"},
                     {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ"},
                     {"role": "system", "content": "制約条件:あなたが一度の会話で答えられる文字数は、150字以内。"},
                     {"role": "system", "content": "制約条件:あなたが説明する時は、あなたが質問を5つ以上行った後とする。"},
                     {"role": "system", "content": "制約条件:相手が「終了」「終わります」と言ったら、あなたは「ありがとうございました」と返す。"},
                     {"role": "system", "content": "制約条件:あなたは相手との会話で[対話型鑑賞力]を評価する。相手の[対話型鑑賞力]を100点満点で点数を付ける。"},
                     {"role": "system", "content": "制約条件:講評として相手の考え方・特徴を述べる。"},
-                    {"role": "system", "content": "以上の命令書と制約条件に従って返信してください。"},
                 ] + conversation_history_1
             }
 
@@ -86,7 +85,7 @@ def page2():
     prompt = ""  # Initialize your prompt
 
     st.title('マルセル・デュシャン「泉」')
-    st.write('好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょ')
+    st.write('<font size="5">好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう。</font>', unsafe_allow_html=True)
     image_1 = Image.open("4.マルセル・デュシャン「泉」.png")
     st.image(image_1, width=400)
     
@@ -108,18 +107,17 @@ def page2():
                 'Content-Type': 'application/json'
             }
             data = {
-                "model": "gpt-3.5-turbo",
+                "model": "gpt-4",
                 "messages": [
-                     {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。相手は[ビジネスパーソン]です。"},
-                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、クライアントに問いかけます。"},
+                    {"role": "system", "content":"命令書:あなたは[対話型鑑賞の専門家]です。"},
+                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、相手に問いかけます。"},
                     {"role": "system", "content": "制約条件:あなたはマルセル・デュシャン「泉」という作品について、相手とやり取りする"},
-                    {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ"},
+                    {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ。"},
                     {"role": "system", "content": "制約条件:あなたが一度の会話で答えられる文字数は、150字以内。"},
                     {"role": "system", "content": "制約条件:あなたが説明する時は、あなたが質問を5つ以上行った後とする。"},
                     {"role": "system", "content": "制約条件:相手が「終了」「終わります」と言ったら、あなたは「ありがとうございました」と返す。"},
                     {"role": "system", "content": "制約条件:あなたは相手との会話で[対話型鑑賞力]を評価する。相手の[対話型鑑賞力]を100点満点で点数を付ける。"},
                     {"role": "system", "content": "制約条件:講評として相手の考え方・特徴を述べる。"},
-                    {"role": "system", "content": "以上の命令書と制約条件に従って返信してください。"},
                      ] + conversation_history_1
             }
 
@@ -142,8 +140,8 @@ def page2():
 def page3():
     prompt = ""  # Initialize your prompt
 
-    st.title('クリスト＆ジャンヌ＝クロード「「L’Arc_de_Triomphe,_Wrapped」')
-    st.write('好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょ')
+    st.title('クリスト＆ジャンヌ＝クロード「L’Arc de Triomphe, Wrapped」')
+    st.write('<font size="5">好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう。</font>', unsafe_allow_html=True)
     image_1 = Image.open("9.クリスト＆ジャンヌ＝クロード「「L’Arc de Triomphe, Wrapped」.jpg")
     st.image(image_1, width=400)
     
@@ -165,18 +163,17 @@ def page3():
                 'Content-Type': 'application/json'
             }
             data = {
-                "model": "gpt-3.5-turbo",
+                "model": "gpt-4",
                 "messages": [
-                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。相手は[ビジネスパーソン]です。"},
-                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、クライアントに問いかけます。"},
-                    {"role": "system", "content": "制約条件:あなたはクリスト＆ジャンヌ＝クロード「「L’Arc_de_Triomphe,_Wrapped」という作品について、相手とやり取りする"},
-                    {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ"},
+                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。"},
+                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、相手に問いかけます。"},
+                    {"role": "system", "content": "制約条件:あなたはクリスト＆ジャンヌ＝クロード「L’Arc　de　Triomphe,　Wrapped」という作品について、相手とやり取りする。"},
+                    {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ。"},
                     {"role": "system", "content": "制約条件:あなたが一度の会話で答えられる文字数は、150字以内。"},
                     {"role": "system", "content": "制約条件:あなたが説明する時は、あなたが質問を5つ以上行った後とする。"},
                     {"role": "system", "content": "制約条件:相手が「終了」「終わります」と言ったら、あなたは「ありがとうございました」と返す。"},
                     {"role": "system", "content": "制約条件:あなたは相手との会話で[対話型鑑賞力]を評価する。相手の[対話型鑑賞力]を100点満点で点数を付ける。"},
                     {"role": "system", "content": "制約条件:講評として相手の考え方・特徴を述べる。"},
-                    {"role": "system", "content": "以上の命令書と制約条件に従って返信してください。"},
                      ] + conversation_history_1
             }
 
@@ -200,14 +197,14 @@ def page3():
 def page4():
     prompt = ""  # Initialize your prompt
 
-    st.title('フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)"')
-    st.write('好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょ')
+    st.title('フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)」')
+    st.write('<font size="5">好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう。</font>', unsafe_allow_html=True)
     image_1 = Image.open("10.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)」.jpg")
     st.image(image_1, width=400)
     
 
     with st.form('qestion_form', clear_on_submit=False):
-        st.markdn('''### 話しかけてみよう!''')
+        st.markdown('''### 話しかけてみよう!''')
         prompt = st.text_area('テキストエリア')
         submitted = st.form_submit_button("送信")
 
@@ -223,18 +220,17 @@ def page4():
                 'Content-Type': 'application/json'
             }
             data = {
-                "model": "gpt-3.5-turbo",
+                "model": "gpt-4",
                 "messages": [
-                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。相手は[ビジネスパーソン]です。"},
-                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、クライアントに問いかけます。"},
-                    {"role": "system", "content": "制約条件:あなたはフェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)という作品について、相手とやり取りする"},
-                    {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ"},
+                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。"},
+                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、相手に問いかけます。"},
+                    {"role": "system", "content": "制約条件:あなたはフェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)」という作品について、相手とやり取りする。"},
+                    {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ。"},
                     {"role": "system", "content": "制約条件:あなたが一度の会話で答えられる文字数は、150字以内。"},
                     {"role": "system", "content": "制約条件:あなたが説明する時は、あなたが質問を5つ以上行った後とする。"},
                     {"role": "system", "content": "制約条件:相手が「終了」「終わります」と言ったら、あなたは「ありがとうございました」と返す。"},
                     {"role": "system", "content": "制約条件:あなたは相手との会話で[対話型鑑賞力]を評価する。相手の[対話型鑑賞力]を100点満点で点数を付ける。"},
                     {"role": "system", "content": "制約条件:講評として相手の考え方・特徴を述べる。"},
-                    {"role": "system", "content": "以上の命令書と制約条件に従って返信してください。"},
                     ] + conversation_history_1
             }
 
@@ -260,7 +256,7 @@ def page5():
     prompt = ""  # Initialize your prompt
 
     st.title('パブロ・ピカソ「アヴィニョンの娘たち」')
-    st.write('#### 好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょ')
+    st.write('<font size="5">好きな絵についてATAI(Art Thinking AI)  と思ったこと/感じたことを話してみましょう。「この絵は明るいね」「よくわからない」など素直にどんどん書き出して会話を楽しみましょう。</font>', unsafe_allow_html=True)
     image_1 = Image.open("2.パブロ・ピカソ「アヴィニョンの娘たち」.jpg")
     st.image(image_1, width=400)
     
@@ -282,10 +278,10 @@ def page5():
                 'Content-Type': 'application/json'
             }
             data = {
-                "model": "gpt-3.5-turbo",
+                "model": "gpt-4",
                 "messages":[
-                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。相手は[ビジネスパーソン]です。"},
-                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、クライアントに問いかけます。"},
+                    {"role": "system", "content": "命令書:あなたは[対話型鑑賞の専門家]です。"},
+                    {"role": "system", "content": "命令書:あなたは以下の制約条件に従って、相手に問いかけます。"},
                     {"role": "system", "content": "制約条件:あなたはパブロ・ピカソ「アヴィニョンの娘たち」という作品について、相手とやり取りする"},
                     {"role": "system", "content": "制約条件:あなたが1度の会話で行う質問は必ず1つずつ"},
                     {"role": "system", "content": "制約条件:あなたが一度の会話で答えられる文字数は、150字以内。"},
@@ -317,28 +313,28 @@ def page5():
 
 page_names_to_funcs = {
     "Main Page": main_page,
-    "1.リクリット・ティラバーニャ「“Who’s_Afraid_of_Red,_Yellow_and_Green,」": page1,
+    "1.リクリット・ティラバーニャ「“Who’s　Afraid　of　Red,　Yellow　and　Green,」": page1,
     "2.マルセル・デュシャン「泉」": page2,
-    "3.クリスト＆ジャンヌ＝クロード「「L’Arc_de_Triomphe,_Wrapped」": page3,
-    "4.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)": page4,
+    "3.クリスト＆ジャンヌ＝クロード「L’Arc　de　Triomphe,　Wrapped」": page3,
+    "4.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)」": page4,
     "5.パブロ・ピカソ「アヴィニョンの娘たち」": page5,
     }
 
-selected_page = st.sidebar.radio("メニュー", ["main_page",
-                                          "1.リクリット・ティラバーニャ「“Who’s_Afraid_of_Red,_Yellow_and_Green,」",
+selected_page = st.sidebar.radio("メニュー", ["main　page",
+                                          "1.リクリット・ティラバーニャ「“Who’s　Afraid　of　Red,　Yellow　and　Green?」",
                                           "2.マルセル・デュシャン「泉」",
-                                          "3.クリスト＆ジャンヌ＝クロード「「L’Arc_de_Triomphe,_Wrapped」",
-                                          "4.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)", 
+                                          "3.クリスト＆ジャンヌ＝クロード「L’Arc　de　Triomphe,　Wrapped」",
+                                          "4.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)」", 
                                           "5.パブロ・ピカソ「アヴィニョンの娘たち」"])
-if selected_page == "main_page":
+if selected_page == "main　page":
     main_page()
-elif selected_page == "1.リクリット・ティラバーニャ「“Who’s_Afraid_of_Red,_Yellow_and_Green,」":
+elif selected_page == "1.リクリット・ティラバーニャ「“Who’s　Afraid　of　Red,　Yellow　and　Green?」":
     page1()
 elif selected_page == "2.マルセル・デュシャン「泉」":
     page2()
-elif selected_page == "3.クリスト＆ジャンヌ＝クロード「「L’Arc_de_Triomphe,_Wrapped」":
+elif selected_page == "3.クリスト＆ジャンヌ＝クロード「L’Arc　de　Triomphe,　Wrapped」":
     page3()
-elif selected_page == "4.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)":
+elif selected_page == "4.フェリックス・ゴンザレス＝トレス「無題(ロスの肖像 L.A.にて)」":
     page4()
 elif selected_page == "5.パブロ・ピカソ「アヴィニョンの娘たち」":
     page5()
